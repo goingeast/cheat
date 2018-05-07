c0e38f90c3e62f0c12255872f58d03deab836c58
Haoru Zhao <haorzhao@amazon.com>
2018-05-06 21:53:25 -0700

    add several basic movement only for iphone 6 plus

--- Cheat.py
+++ Cheat.py
@@ -7,15 +7,22 @@ import random
 import json
 from PIL import Image, ImageDraw
 import wda
+from enum import Enum
+import cv2
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+
+from matplotlib import pyplot as plt
 
 
-with open('config.json', 'r') as f:
-    config = json.load(f)
 
 VERSION = "0.0.1"
 c = wda.Client()
 s = c.session()
-
+print("设备尺寸", s.window_size())
+print s.orientation
+s.orientation = wda.LANDSCAPE
 screenshot_backup_dir = 'screenshot_backups/'
 
 if not os.path.isdir(screenshot_backup_dir):
@@ -31,16 +38,239 @@ def jump(distance):
     print('press time: {}'.format(press_time))
     s.tap_hold(random.uniform(0, 320), random.uniform(64, 320), press_time)
 
+def press_quest():
+    print("Quest button pressed")
+    s.tap_hold(40, 390, 0.5)
+    time.sleep(random.uniform(0.9, 1.5))
+    #s.tap_hold(30,30, 0.5)
+    #time.sleep(random.uniform(0.9, 1.5))
+   # s.tap_hold(80, 360, 0.5)
+
+def go_to_quest():
+    print("Go to Quest button pressed")
+    s.tap_hold(500, 100, 0.5)
+    time.sleep(random.uniform(0.9, 1.5))
+    s.tap_hold(368, 252, 0.5)
+    
+
+def press_rank():
+    print("Map button pressed")
+    s.tap(500, 390)
+    time.sleep(random.uniform(0.9, 1.5))
+    s.tap_hold(30,30, 0.5)
+    time.sleep(random.uniform(0.9, 1.5))
+
 def press_map():
     print("Map button pressed")
-    c.tap(10, 10)
+    s.tap_hold(120, 390, 0.5)
+    time.sleep(random.uniform(0.9, 1.5))
+    s.tap_hold(30,30, 0.5)
+    time.sleep(random.uniform(0.9, 1.5))
 
+def press_home():
+    print("Home button pressed")
+    s.tap_hold(200, 390, 0.5)
+    time.sleep(random.uniform(0.9, 1.5))
+    s.tap_hold(30,30, 0.5)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_up_left():
+    print("up left pressed")
+    s.tap(380-70, 200-70)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_up_right():
+    print("down right pressed")
+    s.tap(380+70, 200-70)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_down_left():
+    print("left down pressed")
+    s.tap(380-70, 200+70)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_down_right():
+    print("right up pressed")
+    s.tap(380+70, 200+70)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_up():
+    print("up pressed")
+    s.tap(380, 200-80)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_down():
+    print("down pressed")
+    s.tap(380, 200+80)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_left():
+    print("left pressed")
+    s.tap(380-80, 200)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_right():
+    print("right pressed")
+    s.tap(380+80, 200)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def getCase():
+    img_rgb = cv2.imread('1.png')
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+    template = cv2.imread('buguanta.jpg',0)
+    w, h = template.shape[::-1]
+ 
+    method = cv2.TM_CCOEFF_NORMED
+
+# resource
+    res = cv2.matchTemplate(img_gray,template,method)
+# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+
+    threshold = 0.8
+    loc = np.where( res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+        return TaskType.CHOCOLATE
+# battle task
+    template = cv2.imread('sure.jpg',0)
+    w, h = template.shape[::-1]
+    method = cv2.TM_CCOEFF_NORMED
+
+
+    res = cv2.matchTemplate(img_gray,template,method)
+    threshold = 0.8
+    loc = np.where( res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+        return TaskType.VANILLA
+
+#  battle
+    template = cv2.imread('attack.jpg',0)
+    w, h = template.shape[::-1]
+    method = cv2.TM_CCOEFF_NORMED
+
+    res = cv2.matchTemplate(img_gray,template,method)
+    threshold = 0.8
+    loc = np.where( res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+        return TaskType.BERRY
+
+    return TaskType.NONE
+
+def press_sure():
+    print("press sure")
+    s.tap(420, 200+45)
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_attack():
+    print("press combat attack")
+    s.tap(420, 330)
+    time.sleep(random.uniform(10, 15))
+    print("press combat plan")
+    s.tap(600, 30) # choose plan
+    time.sleep(random.uniform(0.9, 1.5))
+    print("press combat plan 6")
+    s.tap(650, 200) #plan 6
+    time.sleep(random.uniform(0.9, 1.5))
+    print("press combat attacking")
+    s.tap(650, 390) #start
+    time.sleep(random.uniform(50, 55))
+    print("press combat attacking complete")
+    s.tap(500, 390) #finish
+    time.sleep(random.uniform(3, 4))
+    print("press combat hero exp")
+    s.tap(400, 390) #finish
+    time.sleep(random.uniform(3, 4))
+    print("press combat loot")
+    s.tap(400, 390) #finish
+    time.sleep(random.uniform(0.9, 1.5))
+
+def press_combat():
+    print("press combat start")
+    s.tap(420, 200+45) #sure
+    time.sleep(random.uniform(0.9, 1.5))
+    press_attack()
+
+class TaskType(Enum):
+    VANILLA = 1
+    CHOCOLATE = 2
+    BERRY = 3
+    NONE = 4
+
+#380, 200
 def main():
+    count = 0
+    success = 0
+    fail = 0
+    pull_screenshot()
+    #im = Image.open("./1.png")
     while True:
-        pull_screenshot()
-        im = Image.open("./1.png")
+        count += 1
+        press_quest()
+        #press_rank()
+        go_to_quest()
 
-        press_map()
+        func_set = press_up_left
+        if fail == 0:
+            time.sleep(random.uniform(80, 90))
+            
+        elif fail == 1:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_up_right
+            
+        elif fail == 2:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_down_right
+        
+        elif fail == 3:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_down_left
+            
+        elif fail == 4:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_up
+        elif fail == 5:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_down
+        elif fail == 6:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_left
+        elif fail == 7:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_right
+        elif fail == 8:
+            time.sleep(random.uniform(1, 2))
+            func_set = press_up_left
+            fail = 0
+
+
+        func_set()
+        time.sleep(random.uniform(1, 1.5))
+        pull_screenshot()
+        time.sleep(random.uniform(5, 10))
+        ttype = getCase()
+
+        if ttype == TaskType.CHOCOLATE:
+            press_sure()
+            fail = 0
+            success += 1
+            print("sucess = " ,success, success*100/count)
+        elif ttype == TaskType.VANILLA:
+            press_combat()
+            fail = 0
+            success += 1
+            print("sucess = " ,success, success*100/count)
+        elif ttype == TaskType.BERRY:
+            press_attack()
+            print("Don't do small task")
+            fail += 1
+        else:
+            print("can not find")
+            fail += 1
+            
+
 
 
 if __name__ == '__main__':
